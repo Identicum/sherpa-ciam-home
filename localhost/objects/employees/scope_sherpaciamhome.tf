83d3254f9e1ded886e24d647ff08add23fdad018
Luciano Agustin Crocco <lucianoacrocco@gmail.com>
2026-03-04 15:57:22 -0300

    Renames Keycloak protocol mapper
    Enhances clarity and specificity by updating the protocol mapper's name.
    The new name 'Sherpa ciam home roles' more accurately reflects its purpose compared to the generic 'groups'.
    
    Relates to CLR-718

--- localhost/objects/employees/scope_sherpaciamhome.tf
+++ localhost/objects/employees/scope_sherpaciamhome.tf
@@ -7,7 +7,7 @@ resource "keycloak_openid_client_scope" "sherpa_ciam_home_roles" {
 resource "keycloak_generic_protocol_mapper" "openid_sherpa_ciam_home_roles" {
   realm_id        = keycloak_realm.realm.id
   client_scope_id = keycloak_openid_client_scope.sherpa_ciam_home_roles.id
-  name            = "groups"
+  name            = "Sherpa ciam home roles"
   protocol        = "openid-connect"
   protocol_mapper = "oidc-usermodel-realm-role-mapper"
   config = {
